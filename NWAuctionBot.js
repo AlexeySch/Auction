ba96a6f2722f4a186e09d635df258d8b196422ee
Alexey Shcheglov <Alexey_Shcheglov@epam.com>
2014-07-24 14:32:18 +0400

    File added

---
+++ NWAuctionBot.js
@@ -0,0 +1,21 @@
+﻿var items = Array;
+items = GetAvailableItemList();
+for(var i = 0; i< items.Lenght;i++)
+{
+    PutLot(items[i]);
+}
+
+function GetAvailableItemList()
+{
+    var items = [];
+    //items= LoadAllItems();
+    items = items.filter(MeetList);
+    return items;
+}
+function PutLot(item)
+{
+    var prices = GetCurrentBids();
+    var buyOut = GetBuyOutPrice(prices);
+    var startBid = GetStartBid(prices, buyOut);
+    MakeLot(item);
+}
